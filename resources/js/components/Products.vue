a6ff23ec70beb713257f6e0e9043be1f7bdba3fa
Otavio ACdeC <gutsgt1@gmail.com>
2025-01-03 09:41:15 -0300

    first commit

---
+++ resources/js/components/Products.vue
@@ -0,0 +1,10 @@
+<script setup>
+    import {ref} from "vue";
+    const name = ref("");
+</script>
+
+<template>
+    <h2>Hello Vue.js</h2>
+    {{ name }}
+    <input type="text" v-model="name">
+</template>
